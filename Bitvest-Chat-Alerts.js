ae8f4d109112cffb2c4cf2bee9f8a6f670ad16a6
CttCJim <31054746+CttCJim@users.noreply.github.com>
2023-01-17 08:10:24 -0700

    Update Bitvest-Chat-Alerts.js

--- Bitvest-Chat-Alerts.js
+++ Bitvest-Chat-Alerts.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Bitvest PM Notifier
+// @name         Bitvest Chat Alerts
 // @namespace    http://tampermonkey.net/
-// @version      1.1.2
-// @description  Notify the user when a PM is received on bitvest.io
+// @version      1.0
+// @description  Notify the user when a particular user chats on bitvest.io
 // @author       CttCJim
 // @match        https://bitvest.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bitvest.io
@@ -12,58 +12,65 @@
 
 (function() {
     'use strict';
-    //get name
-    var me = document.getElementsByClassName("self-username")[0].innerHTML;
+    //get names
+    const namelist = [ //list of names to watch for; not case-sensitive
+        'promo',
+        'zodiac',
+        'lightlord'
+    ];
+    //var me = document.getElementsByClassName("self-username")[0].innerHTML;
     //-----------------------------------
     function getLastMsg(highest=0) {
     //get most recent chat DM
-        var allchats = document.getElementsByClassName("pm-body");
+        var allchats = document.getElementsByClassName("msg");
         for(var i=0;i<allchats.length;i++) {
-            //get parent
-            var parentbox = allchats[i].parentElement;
-            //check sender != me
-            var firsttag = parentbox.getElementsByTagName('a')[0];
+            //get index
+            var thisid = allchats[i].id.replace('chat_','');
             //var senderlink = msgbox.firstChild.href.split("#");
-            var senderlink = firsttag.href.split("#");
-            var sendername = senderlink[senderlink.length-1].split(",")[1]; //gets username, aka RainBot
-            if(sendername==me) {continue;} else {
-                var thisid = parentbox.id.replace("chat_","");
-                if(Number(thisid)>highest){
-                    highest=Number(thisid);
-                }
+            if(Number(thisid)>highest){
+                highest=Number(thisid);
             }
         }
         //now we have the number of the most recent PM
         return highest;
     }
-    var oldhighest = 0;
+    var oldhighest = getLastMsg();
     setInterval(function(){
-        var newhighest = getLastMsg(oldhighest); //get most recent PM's index
-        if(document.hasFocus()) {
-            oldhighest=newhighest;
-            return;
-        } //abort if you are already in the window
-        if(oldhighest==0) { //script just started; initialize and don't alert
-            oldhighest=newhighest;
-            //do nothing
-        }
-        if(oldhighest!=newhighest) { //latest message is a new one
-            oldhighest=newhighest;
-            //get the message
-            var msgbox = document.getElementById("chat_"+newhighest);
-            //var msgtext = msgbox.lastChild.innerHTML;
-            var msgtext = msgbox.getElementsByClassName('pm-body')[0].innerHTML;
+        if(oldhighest==0){return;}
+        if(document.hasFocus()) {return;} //abort if you are already in the window
+        var newhighest = getLastMsg();
+        if(oldhighest==newhighest) {return;}
+        var allchats = document.getElementsByClassName("msg");
+        for(var i=oldhighest+1;i<=newhighest;i++) {
+            //check message
+            if(typeof(document.getElementById("chat_"+i))=="undefined") {continue;} //if chat message doesnt exist (ie this index was in a channel you dont have access to)
+            if(document.getElementById("chat_"+i)==null) {continue;} //if chat message doesnt exist (ie this index was in a channel you dont have access to)
+            var msgbox = document.getElementById("chat_"+i);
             var firsttag = msgbox.getElementsByTagName('a')[0];
-            //var senderlink = msgbox.firstChild.href.split("#");
             var senderlink = firsttag.href.split("#");
             var sendername = senderlink[senderlink.length-1].split(",")[1];
-            sendername = sendername.replace("%20"," ");
-            //forward the message to GM_notifiction
-            GM_notification({
-                text: msgtext,
-                title: "New Message from "+sendername,
-                onclick: () => {/*alert('Message clicked')*/}
-            });
+            sendername = sendername.replace("[TIP]%20"," ");//.toLowerCase()
+            for(var j=0;j<namelist.length;j++) {
+                var thisname = namelist[j].toLowerCase();
+                if(thisname==sendername.toLowerCase()) {
+                    var msgtext = "";
+                    if(msgbox.getElementsByClassName('pm-body').length>0) {
+                        msgtext = msgbox.getElementsByClassName('pm-body')[0].innerHTML;
+                    } else {
+                        //remove username and present the text content
+                        msgtext = msgbox.textContent.split(": ");
+                        msgtext.shift();
+                        msgtext = msgtext.join(": ");
+                    }
+                    //forward the message to GM_notifiction
+                    GM_notification({
+                        text: msgtext,
+                        title: "New Chat from "+sendername,
+                        onclick: () => {/*alert('Message clicked')*/}
+                    });
+                }
+            }
         }
+        oldhighest=newhighest;
     },1000);
 })();
